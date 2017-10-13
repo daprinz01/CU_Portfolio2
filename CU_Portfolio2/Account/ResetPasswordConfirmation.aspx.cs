1ffaf2f2b81e2ea63197dfee0db495ae6066ebcd
Okechukwu Prince <daprinz.op@gmail.com>
2017-10-13 23:23:01 +0100

    Add project files.

---
+++ CU_Portfolio2/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace CU_Portfolio2.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ CU_Portfolio2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CU_Portfolio2.Global" Language="C#" %>
